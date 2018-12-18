7f2fbfe504c90fce691d1cef5fdc10e3e1c4a66c
BilalyC <bilaly.camara@outlook.com>
2018-12-18 14:01:20 +0100

    Exercice 1 Complet

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exo1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] week = new string[] { "Lundi", "Mardi", "Mercredi", "Jeudi", "Vendredi", "Samedi", "Dimanche" };
+            /*string[] week = new string[7];
+             * week[0] = "Lundi";
+             * week[1] = "Mardi";
+             * week[2] = "Mercredi";
+             * week[3] = "Jeudi";
+             * week[4] = "Vendredi";
+             * week[5] = "Samedi";
+             * week[6] = "Dimanche";
+             **/
+            for (int i = 0; i < week.Length; i++)
+            {
+                Console.WriteLine(week[i]);
+            }
+        }
+    }
+}
